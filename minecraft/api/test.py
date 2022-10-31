c56c8ffb71a48ca0f56ed788b552db902e7466c4
ekh <52539130+eomkyeongho@users.noreply.github.com>
2022-11-01 02:31:20 +0900

    init

---
+++ minecraft/api/test.py
@@ -0,0 +1,7 @@
+from mcpi.minecraft import Minecraft
+import time
+
+mc = Minecraft.create()
+pos = mc.player.setTilePos(10,70,12)
+
+
---
+++ minecraft/start.bat
@@ -0,0 +1,3 @@
+@echo off
+java -Xms1024M -Xms1024M -jar spigot-1.19.2.jar
+pause
